625e25de7d70b1d1cc6e3c9a6a366096c3ddefa0
Son HyoJae <sonjw5128@gmail.com>
2023-03-23 01:36:05 +0900

    Step1 - 연료 주입 (#1)
    * update: java version setting
    
    * feat: car 추상 클래스 구현
    
    * feat: RentCompany 클래스 구현
    
    * test: RentCompany test
    
    * test: Car test

---
+++ src/main/java/fuel/domain/Avante.java
@@ -0,0 +1,11 @@
+package fuel.domain;
+
+public class Avante extends Car {
+
+    private static final String CAR_NAME = "Avante";
+
+    public Avante(int travelDistance) {
+        super(CAR_NAME, 15, travelDistance);
+    }
+
+}
---
+++ src/main/java/fuel/domain/Sonata.java
@@ -0,0 +1,10 @@
+package fuel.domain;
+
+public class Sonata extends Car {
+
+    private static final String CAR_NAME = "Sonata";
+
+    public Sonata(int travelDistance) {
+        super(CAR_NAME, 10, travelDistance);
+    }
+}
