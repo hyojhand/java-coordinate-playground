4616cd4cf40a4b180986652864193cd5259cfb7e
손효재 <sonjw5128@gmail.com>
2023-03-26 02:57:38 +0900

    feat: 직선 구현 클래스

---
+++ src/main/java/coordinate/domain/Line.java
@@ -0,0 +1,16 @@
+package coordinate.domain;
+
+public class Line extends Figure {
+
+    public static final int COORDINATE_SIZE = 2;
+
+    public Line(Coordinates coordinates) {
+        super(coordinates);
+    }
+
+    @Override
+    public double calculateArea() {
+        return coordinates.getCoordinateIndexOf(0)
+                .calculateDistance(coordinates.getCoordinateIndexOf(1));
+    }
+}
